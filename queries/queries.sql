3f59c4bda2bebdf0b6990f256af699cdcd7982b3
clara-cdp <clarianne.cdp@gmail.com>
2026-01-03 09:54:44 +0100

    Refactor SQL query to select specific columns

--- queries/queries.sql
+++ queries/queries.sql
@@ -5,7 +5,12 @@ SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-SELECT * FROM producto;
+SELECT 
+	codigo,
+    nombre,
+    precio,
+    codigo_fabricante
+FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
 SELECT 
