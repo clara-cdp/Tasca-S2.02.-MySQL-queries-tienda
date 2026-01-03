3daa1bef738c95da5183a0c5005238a7658f41d4
clara-cdp <clarianne.cdp@gmail.com>
2026-01-03 09:48:14 +0100

    Add SQL query to list product names in uppercase

--- queries/queries.sql
+++ queries/queries.sql
@@ -22,10 +22,16 @@ FROM producto;
 FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
-
+SELECT 
+	UPPER(nombre), 
+	precio
+FROM producto;
 
 -- 7. Llista els noms (nombre) i els preus (precio) dels productes de la taula producto, convertint els noms a minúscula.
-
+SELECT 
+	LOWER(nombre), 
+	precio
+FROM producto;
 
 -- 8. Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant (iniciales).
 
